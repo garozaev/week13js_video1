5f377bd153a08827beefac663c2eecf7794fbd26
garozaev <g.a.rozaev@gmail.com>
2023-12-13 13:41:34 +0300

    finish

--- w13jsvideo1.js
+++ w13jsvideo1.js
@@ -8,8 +8,27 @@ console.log(timetamp);// покажет на сутки больше так ка
 let date = new Date('2023-12-12');// запись даты строкой
 console.log(date);
 
-let num = Date(2023,11,12);// выводим через (,) можно указать часы через запятую
+let num = new Date(2023,11,12);// выводим через (,) можно указать часы через запятую
 console.log(num);
 
-let date2 = (`${num.getDate()},${num.getMonth()},${num.getFullYear()}`);
+console.log(num.getDate());
+
+let date2 = `${num.getDate()}.${num.getMonth() + 1}.${num.getFullYear()}`;
 console.log(date2);
+
+num.setFullYear(2024);
+console.log(num);// можно поменять прямо старую переменную,
+// не создовая новую, год в ней поменяется, это особенность new Date
+
+//способ выведения даты
+var options = { year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                timezone: "UTC"};
+console.log(new Date().toLocaleString('ru', options));
+
+//делаем разницу между датами//new Date() и Date.now() одинаково выводят текю дату
+let date3 = new Date('2023-11-01'); // в английском стиле например(у многих стран так)
+let diff = Date.now() - date3;
+console.log(diff);// покажет разницу в мили секундах(исчисление с 01.01.1970 timestemp)
+console.log(diff/ 1000 / 3600);// получим сколько часов прошло
